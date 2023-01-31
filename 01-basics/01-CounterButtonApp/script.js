3bb81b0b66aae1f89de2256ca947f3af84dfa076
Natal D <for_sna@mail.ru>
2023-01-31 18:27:21 +0300

    Две задачи  первой темы

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,20 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+//import { createApp } from 'vue.esm-browser.js';
+
+//import * as Vue from './vue.esm-browser.js';
+
+
 
 // Создайте Vue приложение
+const App = {
+ data() {	
+	return {
+		counter: 0,
+	} 
+ }
+ 
+} //app
+
+//Vue.createApp(App).mount("#app")
+const app = createApp(App);
+const vm = app.mount("#app");
--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,25 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = {
+ data() {	
+	return {
+		inputOne: 0,
+		inputTwo: 0,
+		selOperator: 'sum'
+	} 
+ },
+ 
+ computed: {
+	Result() {	
+		if (this.selOperator === 'sum') { return (this.inputOne + this.inputTwo) }
+			else if (this.selOperator == 'subtract') { return (this.inputOne - this.inputTwo) }
+				else if (this.selOperator == 'multiply') { return (this.inputOne * this.inputTwo) }
+					else if (this.selOperator == 'divide') { return (this.inputOne / this.inputTwo) }
+						else return 0;
+	} 
+ }
+} //App
+
+
+const app = createApp(App);
+const vm = app.mount("#app");
