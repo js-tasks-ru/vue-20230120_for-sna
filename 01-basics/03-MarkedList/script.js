78273e53d948f6cb16968433891ef882475c9ec5
Natal D <for_sna@mail.ru>
2023-02-07 18:47:55 +0300

    3-я задача и на половину 4-я

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,32 +1,53 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
 const emails = [
-  'Eliseo@gardner.biz',
-  'Jayne_Kuhic@sydney.com',
-  'Nikita@garfield.biz',
-  'Lew@alysha.tv',
-  'Hayden@althea.biz',
-  'Presley.Mueller@myrl.com',
-  'Dallas@ole.me',
-  'Mallory_Kunze@marie.org',
-  'Meghan_Littel@rene.us',
-  'Carmen_Keeling@caroline.name',
-  'Veronica_Goodwin@timmothy.net',
-  'Oswald.Vandervort@leanne.org',
-  'Kariane@jadyn.tv',
-  'Nathan@solon.io',
-  'Maynard.Hodkiewicz@roberta.com',
-  'Christine@ayana.info',
-  'Preston_Hudson@blaise.tv',
-  'Vincenza_Klocko@albertha.name',
-  'Madelynn.Gorczany@darion.biz',
-  'Mariana_Orn@preston.org',
-  'Noemie@marques.me',
-  'Khalil@emile.co.uk',
-  'Sophia@arianna.co.uk',
-  'Jeffery@juwan.us',
-  'Isaias_Kuhic@jarrett.net',
-];
+			{ name: 'Eliseo@gardner.biz', f: 0 },
+			{ name: 'Jayne_Kuhic@sydney.com', f: 0 },
+			{ name: 'Nikita@garfield.biz', f: 0 },
+			{ name: 'Lew@alysha.tv', f: 0 },
+			{ name: 'Hayden@althea.biz', f: 0 },
+			{ name: 'Presley.Mueller@myrl.com', f: 0 },
+			{ name: 'Dallas@ole.me', f: 0 },
+			{ name: 'Mallory_Kunze@marie.org', f: 0 },
+			{ name: 'Meghan_Littel@rene.us', f: 0 },
+			{ name: 'Carmen_Keeling@caroline.name', f: 0 },
+			{ name: 'Veronica_Goodwin@timmothy.net', f: 0 },
+			{ name: 'Oswald.Vandervort@leanne.org', f: 0 },
+			{ name: 'Kariane@jadyn.tv', f: 0 },
+			{ name: 'Nathan@solon.io', f: 0 },
+			{ name: 'Maynard.Hodkiewicz@roberta.com', f: 0 },
+			{ name: 'Christine@ayana.info', f: 0 },
+			{ name: 'Preston_Hudson@blaise.tv', f: 0 },
+			{ name: 'Vincenza_Klocko@albertha.name', f: 0 },
+			{ name: 'Madelynn.Gorczany@darion.biz', f: 0 },
+			{ name: 'Mariana_Orn@preston.org', f: 0 },
+			{ name: 'Noemie@marques.me', f: 0 },
+			{ name: 'Khalil@emile.co.uk', f: 0 },
+			{ name: 'Sophia@arianna.co.uk', f: 0 },
+			{ name: 'Jeffery@juwan.us', f: 0 },
+			{ name: 'Isaias_Kuhic@jarrett.net', f: 0 }
+		]
 
-// Требуется создать Vue приложение
+const App = {
+ data() {	
+	return {		
+		ems: [...emails], 
+		inputTxt: ''
+	}
+ },
+
+ computed: {
+		t() {	
+			return this.ems.map(em => {
+				em.f = ( this.inputTxt.length > 0 && em.name.includes(this.inputTxt) > 0 ) ? 1: 0;
+			})
+
+		} 
+ } 
+
+} //app
+
+
+const app = createApp(App);
+const vm = app.mount("#app");
--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,49 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
-
 // Требуется создать Vue приложение
+
+const App = {
+ data() {	
+	return {		
+		meetaps: [1,2,3,4,5], 
+		selMeetup: 1
+	}
+ },
+
+ watch: {
+	selMeetup(newValue, oldValue) 	{
+		console.log(newValue, oldValue);
+		
+		let res = await this.fetchMeetupById(newValue);
+	
+		console.log(res.title);
+	},
+	
+ },
+
+methods: {
+	fetchMeetupById(meetupId) {
+			  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+				if (response.ok) {
+				  return response.json();
+				} else {
+				  return response.json().then((error) => {
+					throw error;
+				  });
+				}
+			  });
+	}	
+},
+
+
+} //app
+
+
+
+const app = createApp(App);
+const vm = app.mount("#app");
+
+
+
