57dd62f86d51d8408fff10eb4c938b99097959f7
Natal D <for_sna@mail.ru>
2023-02-08 22:35:41 +0300

    Вторая тема 4-я задача

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -2,48 +2,45 @@ import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
+function fetchMeetupById(meetupId)  {
+	return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+	if (response.ok) {
+	  return response.json();
+	} else {
+	  return response.json().then((error) => {
+		throw error;
+	  });
+	}
+	});
+}	
+
 // Требуется создать Vue приложение
 
 const App = {
  data() {	
 	return {		
 		meetaps: [1,2,3,4,5], 
-		selMeetup: 1
+		selIdMeetup: 1,
+		selTitleMeetup: ''		
 	}
  },
 
- watch: {
-	selMeetup(newValue, oldValue) 	{
-		console.log(newValue, oldValue);
-		
-		let res = await this.fetchMeetupById(newValue);
-	
-		console.log(res.title);
-	},
-	
- },
+ watch: {	 
+	selIdMeetup: {
+		immediate: true,
+		handler(newValue, oldValue) 	{		
+			console.log(newValue, oldValue);
+			this.selIdMeetup =  newValue;
 
-methods: {
-	fetchMeetupById(meetupId) {
-			  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-				if (response.ok) {
-				  return response.json();
-				} else {
-				  return response.json().then((error) => {
-					throw error;
-				  });
-				}
-			  });
+			let f = fetchMeetupById(this.selIdMeetup).then( meetups => {
+				this.selTitleMeetup = meetups.title;
+				console.log('meetups:');				
+				console.log(meetups.title);	
+			} )
+		}
 	}	
-},
-
-
+ }
 } //app
-
-
 
 const app = createApp(App);
 const vm = app.mount("#app");
-
-
-
